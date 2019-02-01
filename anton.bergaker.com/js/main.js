7e44f1c978a1451ce26bd2a8878b872c2988aad1
Anton <noten.b@hotmail.com>
2019-02-01 01:47:35 +0100

    updated js

--- anton.bergaker.com/js/main.js
+++ anton.bergaker.com/js/main.js
@@ -1,26 +1,15 @@
 // BLERGH IGNORE
-var importedItems = document.getElementsByClassName("imported");
-for (var i=0;i<importedItems.length;i++) {
-    var item = importedItems[i];
-    var ajax = new XMLHttpRequest();
+let importedItems = document.getElementsByClassName("imported");
+for (let i=0;i<importedItems.length;i++) {
+    let item = importedItems[i];
+     let ajax = new XMLHttpRequest();
     
     ajax.open("GET", item.innerHTML, false);
     ajax.send();
     item.outerHTML = ajax.responseText;
 }
 // BLERGH IGNORE END
-
-var reveal_apps = function() {
-    var box = document.getElementsByClassName("app_popup")[0];
-    var is_active = box.style.display == "block";
-    box.style.display = is_active ? "none" : "block";
-}
-
-window.onload = function() {
-    document.getElementById("app_popup_button").onclick = reveal_apps;
-}
-
-var motto = [
+let mottos = [
     "Because programming needs spreadsheets",
     "Anton <3 coffee",
     "When life gives you lemons",
@@ -32,5 +21,5 @@ var motto = [
     "Let's add it to a list! Let's add the list to a list!",
     "You can do it 🎉"
 ];
-var motto = document.getElementById("motto");
+let motto = document.getElementById("motto");
 motto.innerHTML = mottos[Math.floor(Math.random()*mottos.length)];
