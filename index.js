6dc5ba6ebec9264541f7e91d59908719cb50fadf
Anton <noten.b@hotmail.com>
2019-10-11 00:45:39 +0200

    Moved to node

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express');
+const path = require('path');
+const app = express();
+const port = 3000;
+
+const myPath = path.join(__dirname, '/anton.bergaker.com');
+
+app.set('view engine', 'pug');
+app.set('views', myPath);
+
+app.use(function (req, res, next) {
+    var filename = path.basename(req.url);
+    var extension = path.extname(filename);
+    if (extension === '') {
+        res.render(filename, {}, function(err, html) {
+            if (err) {
+                console.log(err);
+                next();
+            } else {
+                res.send(html);
+            }
+        });
+    } else {
+        next();
+    }
+});
+app.use(express.static(myPath));
+
+app.listen(port, () => console.log(`Example app listening on port ${port}!`))
