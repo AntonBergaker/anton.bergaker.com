96a82fc60c95b4734686ab3e2cb73b00070a6769
Anton <noten.b@hotmail.com>
2021-04-30 01:11:10 +0200

    🧨 Added xtreme xplosive fishing

--- make.ps1
+++ make.ps1
@@ -1,4 +1,16 @@
+echo "Removing old files"
 Remove-Item -path out -Recurse
-Copy-Item .\anton.bergaker.com out -Recurse -Exclude .vscode/*
+New-Item -Name "out" -ItemType "directory"
+
+echo "Copying files"
+Copy-Item -Path anton.bergaker.com\* -Destination out -Recurse -Force
+
+echo "Compiling pug"
 pug out
 ls out\*.pug -Recurse | foreach {rm $_}
+
+echo "Copying external files"
+Copy-Item -Path externally_made_stuff\* -Destination out -Recurse -Force
+
+echo "Crushing png images"
+pngquant
